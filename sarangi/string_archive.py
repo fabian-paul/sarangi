c6b1bab5167b63a1e74a73604d825e342a17bad2
Fabian Paul <fab@physik.tu-berlin.de>
2019-01-24 17:11:21 -0600

    added support for cartesian Umbrella centers saved as normal MD trajectories

--- sarangi/string_archive.py
+++ sarangi/string_archive.py
@@ -3,6 +3,7 @@
 import mdtraj
 import os
 import shutil
+import warnings
 
 def save_coor(traj, fname, frame=0):
     from struct import pack
@@ -37,8 +38,9 @@ def load_plan(fname_plan, sim_id):
         plan = yaml.load(f)
     return plan['strings'][0] # TODO
 
+
 def store(plan, fname_trajectory, fname_colvars_traj, sim_id):
-    # TODO: export to command line interface
+    # TODO: export to command line interface (???)
     root = os.environ['STRING_SIM_ROOT']
     branch, iteration, image_major, image_minor = sim_id.split('_')
     fname_archived = '{root}/strings/{branch}_{iteration:03d}/{branch}_{iteration:03d}_{image_major:03d}_{image_minor:03d}'.format(  # TODO: also have this as an environment variable...
@@ -62,7 +64,34 @@ def extract(plan, sim_id, fname_dest_coor, fname_dest_box, top):
     save_xsc(frame, fname_dest_box)
 
 
+def write_image(plan, sim_id, fname_dest_pdb, top_fname):
+    root = os.environ['STRING_SIM_ROOT']
+    me = next(i for i in plan['images'] if i['id'] == sim_id)
+    if 'atoms_1' not in me:
+        warnings.warn('String archivist was instructed to write an image file but the plan does not contain an atom '
+                      'reference. Skipping this step.')
+        return
+
+    prev_id = me['prev_image_id']
+    branch, iteration, image_major, image_minor = prev_id.split('_')
+    fname_dcd = '{root}/strings/{branch}_{iteration:03d}/{branch}_{iteration:03d}_{image_major:03d}_{image_minor:03d}.dcd'.format(
+        root=root, branch=branch, iteration=int(iteration), image_major=int(image_major), image_minor=int(image_minor)
+    )
+    frame_index = me['prev_frame_number']  # TODO: change me?
+    frame = mdtraj.load_frame(fname_dcd, index=frame_index, top=top_fname)
+
+    atoms = me['atoms_1']
+    b_factors = [1 if (i + 1) in atoms else 0 for i in range(frame.top.n_atoms)]
+    frame.save_pdb(fname_dest_pdb, bfactors=b_factors)
+
+
 def write_colvar(colvars_file, colvars_template, sim_id, plan):
+    if not os.path.exists(colvars_template):
+        warnings.warn('String archivist was instructed to write create an colvar input file but not template was found.'
+                      ' Skipping this step.')
+        return
+
+    # TODO: if we use RMSD with changing atoms these would have be written as well to the colvar file atoms { atomsset {...} }
     me = next(i for i in plan['images'] if i['id']==sim_id)
     with open(colvars_template) as f:
         config = ''.join(f.readlines()) + '\n'
@@ -99,19 +128,23 @@ if __name__ == '__main__':
     extractor.add_argument('--id', metavar='code', default='$STRING_IMAGE_ID',
                            help='simulation ID')
     extractor.add_argument('--plan', metavar='path', default='$STRING_PLAN',
-                           help='path to plan file')
+                           help='(in) path to plan file')
     extractor.add_argument('--colvars_template', metavar='path',
-                        default='$STRING_SIM_ROOT/setup/colvars.template',
-                        help='file name of the colvar template')
+                           default='$STRING_SIM_ROOT/setup/colvars.template',
+                           help='(in) file name of the colvar template')
     extractor.add_argument('--colvars', metavar='path',
-                        default='colvars.in',
-                        help='file name of the colvar template')
+                           default='colvars.in',
+                           help='(out) file name of the colvar configuration file')
     extractor.add_argument('--coordinates', metavar='path', default='in.coor',
-                        help='file name to which the restart coordinates are extracted')
+                           help='(out) file name to which the restart coordinates are extracted')
     extractor.add_argument('--box', metavar='path', default='in.xsc',
-                        help='file name to which the restart box information is extracted')
+                           help='(out) file name to which the restart box information is extracted')
     extractor.add_argument('--top', metavar='path', default='$STRING_SIM_ROOT/setup/system.pdb',
-                        help='file name of topology')
+                           help='(in) file name of topology')
+    extractor.add_argument('--image', metavar='path',
+                           default='image.pdb',
+                           help='(out) file name for the image coordinated')
+
 
     args = parser.parse_args()
 
@@ -123,8 +156,12 @@ if __name__ == '__main__':
         store(plan=plan, sim_id=sim_id, fname_trajectory=args.trajectory, fname_colvars_traj=args.colvarstraj)
     elif args.command == 'extract':
         sim_id = os.path.expandvars(args.id)
+        top = os.path.expandvars(args.top)
         plan = load_plan(fname_plan=os.path.expandvars(args.plan), sim_id=sim_id)
-        extract(plan=plan, sim_id=sim_id, fname_dest_coor=args.coordinates, fname_dest_box=args.box, top=os.path.expandvars(args.top))
+        extract(plan=plan, sim_id=sim_id, fname_dest_coor=args.coordinates, fname_dest_box=args.box, top=top)
+
         write_colvar(plan=plan, sim_id=sim_id, colvars_file=args.colvars, colvars_template=os.path.expandvars(args.colvars_template))
+
+        write_image(plan=plan, sim_id=sim_id, fname_dest_pdb=args.image, top_fname=top)
 
     print('end achivist')
