c2ff227a425e1ba16e2c978eefccf106a88ceaa1
Fabian Paul <fapa@midway-login2.rcc.local>
2019-08-15 19:08:38 -0500

    [generation] experimenting with logsumexp

--- sarangi/ext/_short_path.c
+++ sarangi/ext/_short_path.c
@@ -25,7 +25,25 @@ static void sigint_off(void) {
 inline double calc_dist(const float * restrict a, const float * restrict b, size_t n, float param) {
     float sum = 0.;
     for(size_t i=0; i<n; i++) sum += (a[i]-b[i])*(a[i]-b[i]);
-    return exp((double)(param*sqrt(sum))) - 1.;
+    /* return exp((double)(param*sqrt(sum))) - 1.; */
+    return expm1((double)(param*sqrt(sum)));
+}
+
+inline double calc_log_dist(const float * restrict a, const float * restrict b, size_t n, float param) {
+    float sum = 0.;
+    for(size_t i=0; i<n; i++) sum += (a[i]-b[i])*(a[i]-b[i]);
+    double c = param*sqrt(sum);
+    //return c + log(-expm1(-c));
+    return c + log1p(-exp(-c));
+}
+
+inline double log_add_exp(double a, double b) {
+    //double c = fmaxf(a, b);
+    //return c + log(exp(a - c) + exp(b - c));
+    if (a > b)
+        return a + log1p(exp(b - a));
+    else
+        return b + log1p(exp(a - b));
 }
 
 void dijkstra_impl(size_t start, size_t stop, size_t T, size_t n, const float * restrict x, double * restrict dist, char * restrict visited, int * restrict pred, float param) {
@@ -43,9 +61,11 @@ void dijkstra_impl(size_t start, size_t stop, size_t T, size_t n, const float * 
 
         for(size_t v=0; v<T; v++) {
             if(!visited[v]) {
-                double d = calc_dist(&x[u*n], &x[v*n], n, param);
+                /*double d = calc_dist(&x[u*n], &x[v*n], n, param);*/
+                double d = calc_log_dist(&x[u*n], &x[v*n], n, param);
                 if(d + dist[u] < dist[v]) {
-                    dist[v] = d + dist[u];
+                    /*dist[v] = d + dist[u];*/
+                    dist[v] = log_add_exp(d, dist[u]);
                     pred[v] = u;
                  }
              }
